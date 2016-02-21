a6a7d0ad635eb86ae4d074dd8a58086d9c4fa9a5
Ján Kovalčik <johny.kovalcik@gmail.com>
2016-02-21 19:08:50 +0100

    simple project v 1.2

--- app/presenters/PostPresenter.php
+++ app/presenters/PostPresenter.php
@@ -25,6 +25,83 @@ class PostPresenter extends Nette\Application\UI\Presenter
 
     public function renderShow($postId)
     {
-        $this->template->post = $this->database->table('posts')->get($postId);
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post) {
+            $this->error('Stranka nebola najdena');
+        }
+        $this->template->post = $post;
+        $this->template->comments = $post->related('comment')->order('created_at DESC');
+    }
+
+    protected function createComponentCommentForm()
+    {
+        $form = new Form;
+
+        $form->addText('name','Meno:')
+            ->setRequired();
+
+        $form->addText('email','Email:')
+            ->setRequired();
+
+        $form->addTextArea('content','Komentar:')
+            ->setRequired();
+
+        $form->addSubmit('send','Publikovat komentar');
+
+        $form->onSuccess[] = array($this, 'commentFormSucceeded');
+
+        return $form;
+    }
+
+    public function commentFormSucceeded($form, $values)
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert(array(
+            'post_id' => $postId,
+            'name' => $values->name,
+            'email' => $values->email,
+            'content' => $values->content,
+        ));
+
+        $this->flashMessage('Dakujem za komentar','success');
+        $this->redirect('this');
+    }
+
+    protected function createComponentPostForm()
+    {
+        $form = new Form;
+        $form->addText('title', 'Titulok:')
+            ->setRequired();
+        $form->addTextArea('content', 'Obsah:')
+            ->setRequired();
+        $form->addSubmit('send', 'Ulozit a publikovat');
+        $form->onSuccess[] = array($this, 'postFormSucceeded');
+
+        return $form;
+    }
+
+    public function postFormSucceeded($form, $values)
+    {
+        $postId = $this->getParameter('postId');
+
+        if ($postId) {
+            $post = $this->database->table('posts')->get($postId);
+            $post->update($values);
+        } else {
+            $post = $this->database->table('posts')->insert($values);
+        }
+
+        $this->flashMessage("Prispevok bol uspesne publikovany.", 'success');
+        $this->redirect('show', $post->id);
+    }
+
+    public function actionEdit($postId)
+    {
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post)  {
+            $this->error('Prispevok nebol najdeny.');
+        }
+        $this['postForm']->setDefaults($post->toArray());
     }
 }
