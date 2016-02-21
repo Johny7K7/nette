572fba5442dcc9a0539ca0b53a7f0af15f004e33
Ján Kovalčik <johny.kovalcik@gmail.com>
2016-02-21 12:49:59 +0100

    simple project v 1.0

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,20 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->prispevky = $this->database->table('prispevky')
+            ->order('vytvorene DESC')
+            ->limit(5);
+    }
+
 }
